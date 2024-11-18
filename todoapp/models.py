13d58b048b9e14a3679225b4c8e9dcca1f1f14e7
Adelana107 <adelana787898@gmail.com>
2024-11-18 16:48:06 +0100

    new project

---
+++ todoapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Mytodo(models.Model):
+    task = models.CharField(max_length=50)
+
+    def __str__(self):
+        return(self.task)
+    
+
+    
